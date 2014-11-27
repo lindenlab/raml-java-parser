9f6a03bf783c35a95c0ca7bcd65cd38b5800d446
Don Kjer <don.kjer@gmail.com>
2014-11-27 00:05:11 +0000

    Fixing parse-raml.sh

--- docker/parse-raml.sh
+++ docker/parse-raml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RAML="$1"
 RAML_FILE="$(basename "$1")"
-RAML_FQN="$(pwd)/${RAML#.}"
+RAML_FQN="$(pwd)/$RAML"
 
 docker run --rm -v "$RAML_FQN":"/$RAML_FILE" lindenlab.com/raml/java-parser "$RAML_FILE"
